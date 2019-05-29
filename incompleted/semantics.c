3a9cd520b5a28c4c7a4e0db664d527af8d04f053
trongnvv <trongnv138@gmail.com>
2019-05-29 14:48:50 +0700

    check type

--- incompleted/semantics.c
+++ incompleted/semantics.c
@@ -112,22 +112,36 @@ Object* checkDeclaredLValueIdent(char* name) {
 
 void checkIntType(Type* type) {
   // TODO
+  if ((type != NULL) && (type->typeClass == TP_INT))
+    return;
+  else error(ERR_TYPE_INCONSISTENCY, currentToken->lineNo, currentToken->colNo);
 }
 
 void checkCharType(Type* type) {
   // TODO
+  if ((type != NULL) && (type->typeClass == TP_CHAR))
+    return;
+  else error(ERR_TYPE_INCONSISTENCY, currentToken->lineNo, currentToken->colNo);
 }
 
 void checkBasicType(Type* type) {
   // TODO
+  if ((type != NULL) && ((type->typeClass == TP_INT) || (type->typeClass == TP_CHAR)))
+    return;
+  else error(ERR_TYPE_INCONSISTENCY, currentToken->lineNo, currentToken->colNo);
 }
 
 void checkArrayType(Type* type) {
   // TODO
+  if ((type != NULL) && (type->typeClass == TP_ARRAY))
+    return;
+  else error(ERR_TYPE_INCONSISTENCY, currentToken->lineNo, currentToken->colNo);
 }
 
 void checkTypeEquality(Type* type1, Type* type2) {
   // TODO
+  if (compareType(type1, type2) == 0)
+    error(ERR_TYPE_INCONSISTENCY, currentToken->lineNo, currentToken->colNo);
 }
 
 
